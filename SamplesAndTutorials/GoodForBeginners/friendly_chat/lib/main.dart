0f99b0003a2617038200780dd21c770ee2d4617b
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-07-09 17:37:11 -0500

    Finished Step 5 (Added UI for compositing messages).
    https://codelabs.developers.google.com/codelabs/flutter#5

--- SamplesAndTutorials/GoodForBeginners/friendly_chat/lib/main.dart
+++ SamplesAndTutorials/GoodForBeginners/friendly_chat/lib/main.dart
@@ -13,13 +13,55 @@ class FriendlyChatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'FriendlyChat',
+      theme: ThemeData(
+        iconTheme: IconThemeData(
+          color: Theme.of(context).accentColor,
+        ),
+      ),
       home: ChatScreen(),
     );
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
+
+  Widget _buildTextComposer() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        children: [
+          Flexible(
+            child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration: InputDecoration.collapsed(
+                hintText: 'Send a message',
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 4.0),
+            child: IconButton(
+              icon: Icon(Icons.send),
+              onPressed: () => _handleSubmitted(_textController.text),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +69,7 @@ class ChatScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('FriendlyChat'),
       ),
+      body: _buildTextComposer(),
     );
   }
 }
