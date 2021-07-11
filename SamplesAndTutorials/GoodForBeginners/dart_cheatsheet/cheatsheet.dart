ac8a2034c209d5f73efaba206168a6463ca721e9
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-07-11 16:33:41 -0500

    Finished Collection literals.

--- SamplesAndTutorials/GoodForBeginners/dart_cheatsheet/cheatsheet.dart
+++ SamplesAndTutorials/GoodForBeginners/dart_cheatsheet/cheatsheet.dart
@@ -1,19 +1,19 @@
 // Contains all exercises from: https://dart.dev/codelabs/dart-cheatsheet
 
-// String interpolation: https://dart.dev/codelabs/dart-cheatsheet#string-interpolation
-// Example: https://dart.dev/codelabs/dart-cheatsheet#code-example
+    // String interpolation: https://dart.dev/codelabs/dart-cheatsheet#string-interpolation
+    // Example: https://dart.dev/codelabs/dart-cheatsheet#code-example
 String stringify(int x, int y) {
   return '${x} ${y}';
 }
 
-// Nullable variables: https://dart.dev/codelabs/dart-cheatsheet#nullable-variables
-// Example: https://dart.dev/codelabs/dart-cheatsheet#code-example-1
+    // Nullable variables: https://dart.dev/codelabs/dart-cheatsheet#nullable-variables
+    // Example: https://dart.dev/codelabs/dart-cheatsheet#code-example-1
 
 String? name = 'Jane';
 String? address;
 
-// Null-aware operators: https://dart.dev/codelabs/dart-cheatsheet#null-aware-operators
-// Example: https://dart.dev/codelabs/dart-cheatsheet#code-example-2
+    // Null-aware operators: https://dart.dev/codelabs/dart-cheatsheet#null-aware-operators
+    // Example: https://dart.dev/codelabs/dart-cheatsheet#code-example-2
 
 String? foo = 'a string';
 String? bar; // = null
@@ -26,8 +26,8 @@ void updateSomeVars() {
   bar ??= 'a string';
 }
 
-// Conditional property access: https://dart.dev/codelabs/dart-cheatsheet#conditional-property-access
-// Example: https://dart.dev/codelabs/dart-cheatsheet#code-example-3
+    // Conditional property access: https://dart.dev/codelabs/dart-cheatsheet#conditional-property-access
+    // Example: https://dart.dev/codelabs/dart-cheatsheet#code-example-3
 
 // This method should return the uppercase version of `str`
 // or null if `str` is null.
@@ -35,3 +35,25 @@ String? upperCaseIt(String? str) {
   // Try conditionally accessing the `toUpperCase` method here.
   return str?.toUpperCase();
 }
+
+    // Collection literals: https://dart.dev/codelabs/dart-cheatsheet#collection-literals
+    // Example: https://dart.dev/codelabs/dart-cheatsheet#code-example-4
+
+// Assign this a list containing 'a', 'b', and 'c' in that order:
+final aListOfStrings = ['a', 'b', 'c'];
+
+// Assign this a set containing 3, 4, and 5:
+final aSetOfInts = {3, 4, 5};
+
+// Assign this a map of String to int so that aMapOfStringsToInts['myKey'] returns 12:
+final aMapOfStringsToInts = {'myKey': 12};
+
+// Assign this an empty List<double>:
+final anEmptyListOfDouble = <double>[];
+
+// Assign this an empty Set<String>:
+final anEmptySetOfString = <String>{};
+
+// Assign this an empty Map of double to int:
+final anEmptyMapOfDoublesToInts = <double, int>{};
+
