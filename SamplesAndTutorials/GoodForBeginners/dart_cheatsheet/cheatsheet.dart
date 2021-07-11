d8d44a9b1faaef59dbd9e4b28d0af8687e896c8c
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-07-11 16:45:01 -0500

    Finished Dart cheatsheet codelab!

--- SamplesAndTutorials/GoodForBeginners/dart_cheatsheet/cheatsheet.dart
+++ SamplesAndTutorials/GoodForBeginners/dart_cheatsheet/cheatsheet.dart
@@ -265,3 +265,25 @@ class IntegerTriple extends IntegerHolder {
   IntegerTriple(this.a, this.b, this.c);
 }
 
+    // Redirecting constructors: https://dart.dev/codelabs/dart-cheatsheet#redirecting-constructors
+    // Example: https://dart.dev/codelabs/dart-cheatsheet#code-example-15
+
+class Color {
+  int red;
+  int green;
+  int blue;
+
+  Color(this.red, this.green, this.blue);
+
+  Color.black() : this(0, 0, 0);
+}
+
+    // Const constructors: https://dart.dev/codelabs/dart-cheatsheet#const-constructors
+    // Example: https://dart.dev/codelabs/dart-cheatsheet#code-example-16
+
+class Recipe {
+  final List<String> ingredients;
+  final int calories;
+  final double milligramsOfSodium;
+  const Recipe(this.ingredients, this.calories, this.milligramsOfSodium);
+}
