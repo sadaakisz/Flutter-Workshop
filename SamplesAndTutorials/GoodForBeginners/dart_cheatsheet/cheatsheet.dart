9f1dbe5d5305450e6d9b26800abbaf9e939ac961
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-07-11 16:29:22 -0500

    Finished String Interpolation.

---
+++ SamplesAndTutorials/GoodForBeginners/dart_cheatsheet/cheatsheet.dart
@@ -0,0 +1,8 @@
+// Contains all exercises from: https://dart.dev/codelabs/dart-cheatsheet
+
+// String interpolation: https://dart.dev/codelabs/dart-cheatsheet#string-interpolation
+// Example: https://dart.dev/codelabs/dart-cheatsheet#code-example
+String stringify(int x, int y) {
+  return '${x} ${y}';
+}
+
