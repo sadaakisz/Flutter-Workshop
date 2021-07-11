24293af1eb01c196e1b4d91cb1a6554b918649f2
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-07-11 16:39:51 -0500

    Finished Exceptions.

--- SamplesAndTutorials/GoodForBeginners/dart_cheatsheet/cheatsheet.dart
+++ SamplesAndTutorials/GoodForBeginners/dart_cheatsheet/cheatsheet.dart
@@ -156,3 +156,36 @@ class MyDataObject {
   }
 }
 
+    // Exceptions: https://dart.dev/codelabs/dart-cheatsheet#exceptions
+    // Example: https://dart.dev/codelabs/dart-cheatsheet#code-example-10
+
+typedef VoidFunction = void Function();
+
+class ExceptionWithMessage {
+  final String message;
+  const ExceptionWithMessage(this.message);
+}
+
+// Call logException to log an exception, and doneLogging when finished.
+abstract class Logger {
+  void logException(Type t, [String? msg]);
+  void doneLogging();
+}
+
+void tryFunction(VoidFunction untrustworthy, Logger logger) {
+  // Invoking this method might cause an exception. Catch and handle
+  // them using try-on-catch-finally.
+  try {
+    untrustworthy();
+  } on ExceptionWithMessage catch(e){
+    logger.logException(e.runtimeType, e.message);
+  } on Exception {
+    logger.logException(Exception);
+  } catch(e) {
+    rethrow;
+  } finally {
+    logger.doneLogging();
+  }
+
+}
+
