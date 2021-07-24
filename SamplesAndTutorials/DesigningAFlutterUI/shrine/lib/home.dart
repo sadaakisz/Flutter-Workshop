7ba3f97304b319eeed29decf0d9720f7d919d4e1
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-07-24 18:56:45 -0500

    Finished Step 5 of MDC-102 (Add cards in a grid).
    https://codelabs.developers.google.com/codelabs/mdc-102-flutter#4

--- SamplesAndTutorials/DesigningAFlutterUI/shrine/lib/home.dart
+++ SamplesAndTutorials/DesigningAFlutterUI/shrine/lib/home.dart
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -23,7 +24,6 @@ class HomePage extends StatelessWidget {
     // TODO: Pass Category variable to AsymmetricView (104)
     return Scaffold(
       appBar: AppBar(
-        //TODO: Add buttons and title (102)
         leading: IconButton(
           icon: Icon(
             Icons.menu,
@@ -55,9 +55,38 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it!'),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 9.0,
+        //TODO: Build a grid of cards (102)
+        children: <Widget>[
+          Card(
+            clipBehavior: Clip.antiAlias,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                AspectRatio(
+                  aspectRatio: 18.0 / 11.0,
+                  child: Image.asset('assets/diamond.png'),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Title'),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      Text('Secondary Text'),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
       // TODO: To avoid keyboard overflow!!!
       resizeToAvoidBottomInset: false,
