9264ed3b770fb5e6a1507c0bd3ebdad5bd7b8fed
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-07-24 09:24:09 -0500

    Finished Step 7 (Shared Axis Transition in Z-Axis).

--- SamplesAndTutorials/DesigningAFlutterUI/material_motion/lib/router.dart
+++ SamplesAndTutorials/DesigningAFlutterUI/material_motion/lib/router.dart
@@ -1,7 +1,7 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:reply/custom_transition_page.dart';
 import 'package:reply/home.dart';
 import 'package:reply/search_page.dart';
 
@@ -47,12 +47,12 @@ class ReplyRouterDelegate extends RouterDelegate<ReplyRoutePath>
             onPopPage: _handlePopPage,
             pages: [
               // TODO: Add Shared Z-Axis transition from search icon to search view page (Motion)
-              const CustomTransitionPage(
+              const SharedAxisTransitionPageWrapper(
                 transitionKey: ValueKey('Home'),
                 screen: HomePage(),
               ),
               if (routePath is ReplySearchPath)
-                const CustomTransitionPage(
+                const SharedAxisTransitionPageWrapper(
                   transitionKey: ValueKey('Search'),
                   screen: SearchPage(),
                 ),
@@ -96,6 +96,33 @@ class ReplySearchPath extends ReplyRoutePath {
 }
 
 // TODO: Add Shared Z-Axis transition from search icon to search view page (Motion)
+
+class SharedAxisTransitionPageWrapper extends Page {
+  const SharedAxisTransitionPageWrapper(
+      {required this.screen, required this.transitionKey})
+      : super(key: transitionKey);
+  final Widget screen;
+  final ValueKey transitionKey;
+
+  @override
+  Route createRoute(BuildContext context) {
+    return PageRouteBuilder(
+      settings: this,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        return SharedAxisTransition(
+          fillColor: Theme.of(context).cardColor,
+          animation: animation,
+          secondaryAnimation: secondaryAnimation,
+          transitionType: SharedAxisTransitionType.scaled,
+          child: child,
+        );
+      },
+      pageBuilder: (context, animation, secondaryAnimation) {
+        return screen;
+      },
+    );
+  }
+}
 
 class ReplyRouteInformationParser
     extends RouteInformationParser<ReplyRoutePath> {
