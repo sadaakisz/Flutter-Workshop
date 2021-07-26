61f335c77555e4a09e21e791d3aa6b3b657a7c1d
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-07-26 15:37:49 -0500

    Finished Step 5 of MDC-104 (Added shapes to the backdrop).

--- SamplesAndTutorials/DesigningAFlutterUI/shrine_mdc_104/lib/backdrop.dart
+++ SamplesAndTutorials/DesigningAFlutterUI/shrine_mdc_104/lib/backdrop.dart
@@ -31,12 +31,14 @@ class _BackdropState extends State<Backdrop>
 
   //TODO: Add BuildContext and BoxConstraints parameters to _buildStack (104)
   Widget _buildStack() {
+    //TODO: Create a RelativeRectTween Animation (104)
     return Stack(
       key: _backdropKey,
       children: <Widget>[
         //TODO: Wrap backLayer in an ExcludeSemantics widget (104)
         widget.backLayer,
-        widget.frontLayer,
+        //TODO: Add a PositionedTransition (104)
+        _FrontLayer(child: widget.frontLayer),
       ],
     );
   }
@@ -79,6 +81,36 @@ class _BackdropState extends State<Backdrop>
       appBar: appBar,
       //TODO: Return a LayoutBuilder widget (104)
       body: _buildStack(),
+    );
+  }
+}
+
+class _FrontLayer extends StatelessWidget {
+  //TODO: Add on-tap callback (104)
+
+  const _FrontLayer({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 16.0,
+      shape: BeveledRectangleBorder(
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(46.0)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          //TODO: Add a gesture detector (104)
+          Expanded(
+            child: child,
+          ),
+        ],
+      ),
     );
   }
 }
