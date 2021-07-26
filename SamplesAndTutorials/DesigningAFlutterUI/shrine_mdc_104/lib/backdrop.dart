c481cb3473ddc30bfb0bd34105d257dd4f27f84d
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-07-26 17:08:08 -0500

    Finished Step 6 of MDC-104 (Added motion on Menu tap).

--- SamplesAndTutorials/DesigningAFlutterUI/shrine_mdc_104/lib/backdrop.dart
+++ SamplesAndTutorials/DesigningAFlutterUI/shrine_mdc_104/lib/backdrop.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'model/product.dart' as product;
 
-//TODO: Add velocity constant (104)
+const double _kFlingVelocity = 2.0;
 
 class Backdrop extends StatefulWidget {
   final product.Category currentCategory;
@@ -27,18 +28,62 @@ class _BackdropState extends State<Backdrop>
     with SingleTickerProviderStateMixin {
   final GlobalKey _backdropKey = GlobalKey(debugLabel: 'Backdrop');
 
-  //TODO: Add Animation Controller widget (104)
+  late AnimationController _controller;
 
-  //TODO: Add BuildContext and BoxConstraints parameters to _buildStack (104)
-  Widget _buildStack() {
-    //TODO: Create a RelativeRectTween Animation (104)
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 300),
+      value: 1.0,
+      vsync: this,
+    );
+  }
+
+  //TODO: Add override for didUpdateWidget (104)
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  bool get _frontLayerVisible {
+    final AnimationStatus status = _controller.status;
+    return status == AnimationStatus.completed ||
+        status == AnimationStatus.forward;
+  }
+
+  void _toggleBackdropLayerVisibility() {
+    _controller.fling(
+      velocity: _frontLayerVisible ? -_kFlingVelocity : _kFlingVelocity,
+    );
+  }
+
+  Widget _buildStack(BuildContext context, BoxConstraints constraints) {
+    const double layerTitleHeight = 48.0;
+    final Size layerSize = constraints.biggest;
+    final double layerTop = layerSize.height - layerTitleHeight;
+
+    Animation<RelativeRect> layerAnimation = RelativeRectTween(
+      begin: RelativeRect.fromLTRB(
+          0.0, layerTop, 0.0, layerTop - layerSize.height),
+      end: RelativeRect.fromLTRB(0.0, 0.0, 0.0, 0.0),
+    ).animate(_controller.view);
     return Stack(
       key: _backdropKey,
       children: <Widget>[
-        //TODO: Wrap backLayer in an ExcludeSemantics widget (104)
-        widget.backLayer,
-        //TODO: Add a PositionedTransition (104)
-        _FrontLayer(child: widget.frontLayer),
+        ExcludeSemantics(
+          child: widget.backLayer,
+          excluding: _frontLayerVisible,
+        ),
+        PositionedTransition(
+          rect: layerAnimation,
+          child: _FrontLayer(
+            //TODO: Implement onTap property on _BackdropState (104)
+            child: widget.frontLayer,
+          ),
+        ),
       ],
     );
   }
@@ -52,7 +97,10 @@ class _BackdropState extends State<Backdrop>
       //TODO: Replace leading menu icon with IconButton (104)
       //TODO: Remove leading property (104)
       //TODO: Create title with _BackdropTitle parameter (104)
-      leading: Icon(Icons.menu),
+      leading: IconButton(
+        icon: Icon(Icons.menu),
+        onPressed: _toggleBackdropLayerVisibility,
+      ),
       title: Text('SHRINE'),
       actions: <Widget>[
         //TODO: Add shortcut to login screen from trailing icons (104)
@@ -80,7 +128,7 @@ class _BackdropState extends State<Backdrop>
     return Scaffold(
       appBar: appBar,
       //TODO: Return a LayoutBuilder widget (104)
-      body: _buildStack(),
+      body: LayoutBuilder(builder: _buildStack),
     );
   }
 }
