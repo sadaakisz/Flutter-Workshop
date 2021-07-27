ae6d132674c33db0eafdf0192a6e5f7b9caa883a
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-07-26 22:31:30 -0500

    Finished MDC-104! (Added branded menu and title interaction).

--- SamplesAndTutorials/DesigningAFlutterUI/shrine_mdc_104/lib/backdrop.dart
+++ SamplesAndTutorials/DesigningAFlutterUI/shrine_mdc_104/lib/backdrop.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'login.dart';
 import 'model/product.dart' as product;
 
 const double _kFlingVelocity = 2.0;
@@ -103,32 +104,35 @@ class _BackdropState extends State<Backdrop>
       brightness: Brightness.light,
       elevation: 0.0,
       titleSpacing: 0.0,
-      //TODO: Replace leading menu icon with IconButton (104)
-      //TODO: Remove leading property (104)
-      //TODO: Create title with _BackdropTitle parameter (104)
-      leading: IconButton(
-        icon: Icon(Icons.menu),
-        onPressed: _toggleBackdropLayerVisibility,
+      title: _BackdropTitle(
+        listenable: _controller.view,
+        onPress: _toggleBackdropLayerVisibility,
+        frontTitle: widget.frontTitle,
+        backTitle: widget.backTitle,
       ),
-      title: Text('SHRINE'),
       actions: <Widget>[
-        //TODO: Add shortcut to login screen from trailing icons (104)
         IconButton(
           icon: Icon(
             Icons.search,
-            semanticLabel: 'search',
+            semanticLabel: 'login',
           ),
           onPressed: () {
-            //TODO: Add open login (104)
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (BuildContext context) => LoginPage()),
+            );
           },
         ),
         IconButton(
           icon: Icon(
             Icons.tune,
-            semanticLabel: 'filter',
+            semanticLabel: 'login',
           ),
           onPressed: () {
-            //TODO: Add open login (104)
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (BuildContext context) => LoginPage()),
+            );
           },
         ),
       ],
@@ -136,15 +140,12 @@ class _BackdropState extends State<Backdrop>
     );
     return Scaffold(
       appBar: appBar,
-      //TODO: Return a LayoutBuilder widget (104)
       body: LayoutBuilder(builder: _buildStack),
     );
   }
 }
 
 class _FrontLayer extends StatelessWidget {
-  //TODO: Add on-tap callback (104)
-
   const _FrontLayer({
     Key? key,
     required this.child,
@@ -174,6 +175,91 @@ class _FrontLayer extends StatelessWidget {
           ),
           Expanded(
             child: child,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BackdropTitle extends AnimatedWidget {
+  final void Function() onPress;
+  final Widget frontTitle;
+  final Widget backTitle;
+
+  const _BackdropTitle({
+    Key? key,
+    required Animation<double> listenable,
+    required this.onPress,
+    required this.frontTitle,
+    required this.backTitle,
+  })  : _listenable = listenable,
+        super(key: key, listenable: listenable);
+
+  final Animation<double> _listenable;
+
+  @override
+  Widget build(BuildContext context) {
+    final Animation<double> animation = _listenable;
+
+    return DefaultTextStyle(
+      style: Theme.of(context).primaryTextTheme.headline6!,
+      softWrap: false,
+      overflow: TextOverflow.ellipsis,
+      child: Row(
+        children: <Widget>[
+          //TODO: Branded icon
+          SizedBox(
+            width: 72.0,
+            child: IconButton(
+              padding: EdgeInsets.only(right: 8.0),
+              onPressed: this.onPress,
+              icon: Stack(children: <Widget>[
+                Opacity(
+                  opacity: animation.value,
+                  child: ImageIcon(AssetImage('assets/slanted_menu.png')),
+                ),
+                FractionalTranslation(
+                  translation: Tween<Offset>(
+                    begin: Offset.zero,
+                    end: Offset(1.0, 0.0),
+                  ).evaluate(animation),
+                  child: ImageIcon(AssetImage('assets/diamond.png')),
+                )
+              ]),
+            ),
+          ),
+          // Here, we do a custom cross fade between backTitle and frontTitle.
+          // This makes a smooth animation between the two texts.
+          Stack(
+            children: <Widget>[
+              Opacity(
+                opacity: CurvedAnimation(
+                        parent: ReverseAnimation(animation),
+                        curve: Interval(0.5, 1.0))
+                    .value,
+                child: FractionalTranslation(
+                  translation: Tween<Offset>(
+                    begin: Offset.zero,
+                    end: Offset(0.5, 0.0),
+                  ).evaluate(animation),
+                  child: backTitle,
+                ),
+              ),
+              Opacity(
+                opacity: CurvedAnimation(
+                  parent: animation,
+                  curve: Interval(0.5, 1.0),
+                ).value,
+                child: FractionalTranslation(
+                  translation: Tween<Offset>(
+                    begin: Offset(-0.25, 0.0),
+                    end: Offset.zero,
+                  ).evaluate(animation),
+                  child: frontTitle,
+                ),
+              ),
+            ],
           ),
         ],
       ),
